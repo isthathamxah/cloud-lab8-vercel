f307f2d1dcc4c5edebe277956a65e85faef2c777
Your Name <you@example.com>
2026-05-23 16:24:00 +0500

    feat: add hello API route

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
+  const name = process.env.NEXT_PUBLIC_STUDENT_NAME;
   return (
     <main style={{ textAlign: "center", marginTop: "10%" }}>
       <h1>Lab 8 : Vercel Deployment</h1>
-      <p>Name: Muhammad Hamza</p>
+      <p>Name: {name}</p>
       <p>Roll: 01-131232-056</p>
     </main>
   );
